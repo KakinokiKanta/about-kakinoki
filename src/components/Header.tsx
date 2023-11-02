662b68abcc3ee1104fb5b66e5417c39ce8daf406
KakinokiKanta <kanta12621@icloud.com>
2023-11-02 14:00:38 +0900

    update: ヘッダーの調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,28 @@ import styles from "./Header.module.css";
 import { AlumiSansCollegiateOne } from "@/lib/fontManager";
 
 export const Header = () => {
+  const HEADER_ITEMS = [
+    { href: "/", label: "HOME" },
+    { href: "/about", label: "ABOUT" },
+    { href: "/etrobo", label: "ETROBOCON" },
+    { href: "/university", label: "UNIVERSITY" },
+    { href: "/volunteer", label: "VOLUNTEER" },
+  ];
+
   return (
     <header>
       <div className={styles.container}>
-        {/* フォントを変える */}
         <div className={`${AlumiSansCollegiateOne.className} ${styles.home}`}>
           Kakinoki Kanta
         </div>
         <div className={styles.items}>
-          <Link href="/" className={styles.link}>
-            Home
-          </Link>
+          {HEADER_ITEMS.map((item) => {
+            return (
+              <Link href={item.href} className={styles.link} key={item.href}>
+                {item.label}
+              </Link>
+            );
+          })}
         </div>
       </div>
     </header>
