b4e00c09a297e80ae1478717d395724b6f45bbed
KakinokiKanta <kanta12621@icloud.com>
2023-11-02 20:27:03 +0900

    update: ホームABOUTの調整

--- src/app/_components/HomeAbout/index.tsx
+++ src/app/_components/HomeAbout/index.tsx
@@ -11,7 +11,10 @@ export const HomeAbout = () => {
         プログラミングは大学から始め、最近はRSCの勉強をしています。
       </p>
       <p className={styles.description}>
-        普段は研究とか、ETロボコンの活動をしています。
+        普段は研究したり、ETロボコンの活動したりって感じです。
+      </p>
+      <p className={styles.description}>
+        趣味はサッカー観戦です。ラリーガや代表戦はフルで観ることも多いです。
       </p>
       <LinkButton href="/about" />
     </section>
