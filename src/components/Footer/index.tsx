75f50278caa005f480cfcaa31b3abfca9db5470f
KakinokiKanta <kanta12621@icloud.com>
2023-11-06 00:27:10 +0900

    update: フッター内アイコンクリックで外部リンクにとべる用に変更

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,44 @@
 import Link from "next/link";
+import Image from "next/image";
 import { HEADER_ITEMS } from "../Header";
 import styles from "./Footer.module.css";
 import { alumiSansCollegiateOne } from "@/lib/fontManager";
 
 export const Footer = () => {
+  const ICONS_PATH = [
+    {
+      image: "/icons/x-logo-black.png",
+      href: "https://twitter.com/kantan_IT",
+      alt: "x icon",
+    },
+    {
+      image: "/icons/github-mark.png",
+      href: "https://github.com/KakinokiKanta",
+      alt: "github icon",
+    },
+  ];
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.vercel}>
           <div className={`${styles.name} ${alumiSansCollegiateOne.className}`}>
-            kakinoki
+            Kakinoki
           </div>
           <div className={styles.subtitle}>SOCIAL</div>
           <div className={styles.icons}>
-            <div className={styles.icon}>Xのアイコン</div>
-            <div className={styles.icon}>githubのアイコン</div>
+            {ICONS_PATH.map((item) => {
+              return (
+                <a href={item.href} className={styles.icon} key={item.image}>
+                  <Image
+                    src={item.image}
+                    width={40}
+                    height={40}
+                    alt={item.alt}
+                  />
+                </a>
+              );
+            })}
           </div>
         </div>
         <div className={styles.links}>
@@ -27,6 +51,9 @@ export const Footer = () => {
             );
           })}
         </div>
+        <p className={styles.copyright}>
+          &copy; {new Date().getFullYear()} Kakinoki
+        </p>
       </div>
     </footer>
   );
