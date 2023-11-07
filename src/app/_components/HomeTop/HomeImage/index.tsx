3de0af54b69abcae0120a0310694edf3d71fe340
KakinokiKanta <kanta12621@icloud.com>
2023-11-08 04:34:58 +0900

    add: HOMEトップ画像を複数追加および数秒おきに切り替えるよう変更

--- src/app/_components/HomeTop/HomeImage/index.tsx
+++ src/app/_components/HomeTop/HomeImage/index.tsx
@@ -1,25 +1,46 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./HomeImage.module.css";
 
 export const HomeImage = () => {
-  const [profileImage, setProfileImage] = useState(
-    "/home_imgs/travel_back.JPG"
-  );
+  const IMAGE_LIST = [
+    "/home_imgs/travel_back.JPG",
+    "/home_imgs/ankake.jpg",
+    "/home_imgs/travel_lufy.JPG",
+    "/home_imgs/balloon.jpg",
+    "/home_imgs/tomato_ramen.jpg",
+  ];
+
+  const [profileImageIndex, setProfileImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProfileImageIndex((prevIndex) => (prevIndex + 1) % IMAGE_LIST.length);
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={styles.parent}>
-      <Image
-        className={styles.image}
-        src={profileImage}
-        alt="Photo for self-introduction"
-        fill
-        style={{
-          objectFit: "cover",
-        }}
-      />
+      {IMAGE_LIST.map((image, index) => {
+        return (
+          <Image
+            className={`${styles.image} ${
+              index === profileImageIndex && styles.active
+            }`}
+            src={image}
+            alt="Photo for self-introduction"
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            key={index}
+          />
+        );
+      })}
     </div>
   );
 };
