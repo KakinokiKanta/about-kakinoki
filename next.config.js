f1efe9752aeb66fe34d3784c1877c467ebdd4776
KakinokiKanta <kanta12621@icloud.com>
2023-11-25 00:44:20 +0900

    update: pdf表示に成功

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
     },
   },
   webpack: (config, options) => {
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[contenthash].[ext]",
+            publicPath: "/_next/static/worker",
+            outputPath: "static/worker",
+          },
+        },
+      ],
+    });
     // Important: return the modified config
     config.module.rules.push({
       test: /\.node/,
--- src/app/etrobo/_components/KatLab/Model/index.tsx
+++ src/app/etrobo/_components/KatLab/Model/index.tsx
@@ -1,13 +1,33 @@
 "use client";
 
 import styles from "./Model.module.css";
-import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+import pdfjsWorkerSrc from "@/../pdf-worker";
+import { useState } from "react";
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorkerSrc;
 
 export const Model = () => {
+  const [numPages, setNumPages] = useState(1);
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
+
   return (
-    <div>
-      <Document file="/robocon/ADV_063_KatLab.pdf">
-        <Page pageNumber={1} />
+    <div className={styles.flame}>
+      <Document
+        file="/robocon/ADV_063_KatLab.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+        className={styles.pdf}
+      >
+        {Array.from({ length: numPages }, (_, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            renderAnnotationLayer={false}
+            renderTextLayer={false}
+          />
+        ))}
       </Document>
     </div>
   );
