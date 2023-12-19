d3ff0dcdf67d25a1f4c377d846220fe627ca5f62
KakinokiKanta <kanta12621@icloud.com>
2023-12-20 02:25:43 +0900

    add: universityページ上部の追加

---
+++ src/app/university/_components/MiyazakiUni/ProEx/index.tsx
@@ -0,0 +1,36 @@
+import styles from "./ProEx.module.css";
+import Image from "next/image";
+
+type ProExProps = {
+  h3: string;
+  img: string;
+  alt: string;
+  p: string[];
+};
+
+export const ProEx = (props: ProExProps) => {
+  return (
+    <div>
+      <h3 className={styles.h3}>{props.h3}</h3>
+      <div className={styles.container}>
+        <div className={styles.parent}>
+          <Image
+            src={props.img}
+            alt={props.alt}
+            fill
+            sizes="(max-width: 768px) 90%, 50%"
+            style={{
+              objectFit: "contain",
+            }}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.text}>
+          {props.p.map((item) => {
+            return <p key={item}>{item}</p>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
