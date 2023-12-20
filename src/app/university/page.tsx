ebb9e06ffe9135e39ee5823d96cdeaf3da31dff2
KakinokiKanta <kanta12621@icloud.com>
2023-12-21 04:36:02 +0900

    add: 研究室に関する説明をするコンポーネントの追加

--- src/app/university/page.tsx
+++ src/app/university/page.tsx
@@ -1,9 +1,11 @@
+import { KatayamaLab } from "./_components/KatayamaLab";
 import { MiyazakiUni } from "./_components/MiyazakiUni";
 
 const University = () => {
   return (
     <main>
       <MiyazakiUni />
+      <KatayamaLab />
     </main>
   );
 };
