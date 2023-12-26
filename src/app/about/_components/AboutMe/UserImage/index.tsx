8bb42b3493a8c2706576d7aaa6ea5c8150a08312
KakinokiKanta <kanta12621@icloud.com>
2023-12-27 00:41:46 +0900

    fix: バグ修正

--- src/app/about/_components/AboutMe/UserImage/index.tsx
+++ src/app/about/_components/AboutMe/UserImage/index.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import Image from "next/image";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "@/lib/apolloClient";
 import styles from "./UserImage.module.css";
 
 export const UserImage = () => {
   return (
-    <div className={styles.parent}>
-      {/* TODO: githubAPI */}
-      <Image
-        src="/home_imgs/travel_back.JPG"
-        alt="Kakinoki profile image"
-        fill
-        style={{ objectFit: "cover" }}
-        sizes="(max-width: 768px) 50%, 30%"
-      />
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className={styles.parent}>
+        {/* TODO: githubAPI */}
+        <Image
+          src="/home_imgs/travel_back.JPG"
+          alt="Kakinoki profile image"
+          fill
+          style={{ objectFit: "cover" }}
+          sizes="(max-width: 768px) 50%, 30%"
+        />
+      </div>
+    </ApolloProvider>
   );
 };
