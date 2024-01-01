a8a81824e4f1a9c16633ec847e8e59f52daa8c71
KakinokiKanta <kanta12621@icloud.com>
2024-01-02 03:18:41 +0900

    update: 認証用のメソッドを使用する用に変更

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,20 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.GITHUB_API_KEY}`,
+    },
+  };
+});
 
 export const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  headers: { authorirization: `Bearer ${process.env.GITHUB_API_KEY}` },
-  uri: "https://api.github.com/graphql",
 });
