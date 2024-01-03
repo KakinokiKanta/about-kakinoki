0cb0bb36172bbf769ad5f552c0316f29dbf9a0af
KakinokiKanta <kanta12621@icloud.com>
2024-01-04 01:13:22 +0900

    fix: 自分についての画像を削除

--- src/app/about/_components/AboutMe/UserImage/index.tsx
+++ src/app/about/_components/AboutMe/UserImage/index.tsx
@@ -29,13 +29,13 @@ export const UserImage = () => {
   return (
     <div className={styles.parent}>
       {/* TODO: githubAPI */}
-      <Image
+      {/* <Image
         src="/home_imgs/travel_back.JPG"
         alt="Kakinoki profile image"
         fill
         style={{ objectFit: "cover" }}
         sizes="(max-width: 768px) 50%, 30%"
-      />
+      /> */}
     </div>
   );
 };
