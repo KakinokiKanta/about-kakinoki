0136bbf51583cafefcf6066e44e8923857f346e0
KakinokiKanta <kanta12621@icloud.com>
2024-01-16 00:42:55 +0900

    add: ローディング画面用の自作コンポーネントを実装

---
+++ src/app/_loading/Loading.tsx
@@ -0,0 +1,24 @@
+import styles from "./Loading.module.css";
+
+const Loading = () => {
+  return (
+    <div>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+      <p>loading...</p>
+    </div>
+  );
+};
