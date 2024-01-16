42a915bbb78c38043eedb13fbfb2ea05554d5ded
KakinokiKanta <kanta12621@icloud.com>
2024-01-16 17:38:39 +0900

    fix: プロダクトコンポーネント内で画像に関する箇所をサーバコンポーネントに移行

---
+++ src/components/Products/ProductContainer.tsx
@@ -0,0 +1,29 @@
+import { Products } from ".";
+import { ProductContent } from "./ProductContent";
+import styles from "./productContainer.module.css";
+
+type ProductContainerProps = {
+  title: string;
+  image: string;
+  alt: string;
+  description: string[];
+  tools: string[];
+  links: {
+    href: string;
+    kinds: string;
+  }[];
+};
+
+export const ProductContainer = (props: ProductContainerProps) => {
+  return (
+    <div className={styles.container}>
+      <Products {...props}>
+        <ProductContent
+          title={props.title}
+          image={props.image}
+          alt={props.alt}
+        />
+      </Products>
+    </div>
+  );
+};
