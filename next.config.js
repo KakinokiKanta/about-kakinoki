556ae175971135679b532a4b7f59c3513107b428
KakinokiKanta <kanta12621@gmail.com>
2025-02-07 03:37:13 +0900

    update: nextの設定ファイルをreact-pdfのバージョン更新に対応

--- next.config.js
+++ next.config.js
@@ -1,32 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
+  swcMinify: false,
   // 以下を追加
-  compiler: {
-    styledComponents: {
-      ssr: true,
-    },
-  },
-  webpack: (config, options) => {
-    config.module.rules.unshift({
-      test: /pdf\.worker\.(min\.)?js/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[contenthash].[ext]",
-            publicPath: "/_next/static/worker",
-            outputPath: "static/worker",
-          },
-        },
-      ],
-    });
+  // compiler: {
+  //   styledComponents: {
+  //     ssr: true,
+  //   },
+  // },
+  webpack: (config) => {
+    // config.module.rules.unshift({
+    //   test: /pdf\.worker\.(min\.)?mjs/,
+    //   use: [
+    //     {
+    //       loader: "file-loader",
+    //       options: {
+    //         name: "[contenthash].[ext]",
+    //         publicPath: "/_next/static/worker",
+    //         outputPath: "static/worker",
+    //       },
+    //     },
+    //   ],
+    // });
+    config.resolve.alias.canvas = false;
     // Important: return the modified config
-    config.module.rules.push({
-      test: /\.node/,
-      use: "raw-loader",
-    });
+    // config.module.rules.push({
+    //   test: /\.node/,
+    //   use: "raw-loader",
+    // });
     return config;
   },
 };
