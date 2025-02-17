fc64d0471f4fb8c98777356c24ff37a66b1cb2bc
KakinokiKanta <kanta12621@gmail.com>
2025-02-17 15:43:33 +0900

    add: 研究に関するリストを追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,8 +14,7 @@ const About = () => {
       "2023/10 : ETロボコン地区(九州)大会2022 アドバンストクラス 総合優勝",
       ["競技 準優勝", "モデル審査 ゴールドモデル", "特別賞 IPA賞"],
       "2023/09 : 株式会社ディー・エヌ・エー(DeNA) オータムハッカソン2023 優勝",
-      "2023/09 : 電気・情報関係学会九州支部連合大会 研究発表",
-      "2023/08 : 楽天グループ株式会社 短期インターンシップ(1week)",
+      // "2023/08 : 楽天グループ株式会社 短期インターンシップ(1week)",
       "2023/03 : 宮崎大学工学部 優秀学生賞",
       "2023/03 : 公益社団法人計測自動制御学会 優秀学生賞",
       "2022/11 : ETロボコンCS(全国)大会2022 アドバンストクラス 総合4位",
@@ -24,6 +23,15 @@ const About = () => {
       "2021/06 : 特定サイバー防犯ボランティア 受嘱",
     ],
   };
+
+  const Thesis = [
+    "2025/02 : Springer Singapore, Genetic and Evolutionary Computing 論文誌投稿",
+    "2024/10 : 宮崎大学工学部紀要 論文誌投稿",
+    "2024/08 : International Conference on Genetic and Evolutionary Computing (ICGEC-2024) 学会発表",
+    "2024/03 : Journal of Robotics, Networking and Artificial Life (JRNAL) 論文誌投稿",
+    "2024/02 : International Conference on Artificial Life and Robotics (ICAROB) 学会発表",
+    "2023/09 : 電気・情報関係学会九州支部連合大会 学会発表",
+  ];
 
   const EXAMS = {
     title: "EXAMS",
